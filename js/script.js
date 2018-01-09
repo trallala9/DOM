14c4d6a2ddd66599961ace16414d73f378da05b7
Karolina Siwkowska <trallala00@gmail.com>
2018-01-09 16:41:13 +0100

    Init project

--- js/script.js
+++ js/script.js
@@ -1,16 +1,4 @@
-var buttonClass = document.getElementsByClassName('button');
+var buttonClass = document.getElementsByClassName("button");
 for (var k = 0; k <= 4; k++) {
-    console.log(buttonClass);
+    alert(buttonClass.innerText);
 }
-var buttonClass = document.getElementsByClassName('button');
-for (var k = 0; k <= 4; k++) {
-    console.log(buttonClass.innerText);
-}
-console.log(document);
-
-
-var itemsByTagName = document.getElementsByTagName('p');
-for (var k = 0; k <= 4; k++) {
-    console.log(document.itemsByTagName);
-}
-console.log(buttonClass.innerText);
